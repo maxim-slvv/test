3400dc12709497d66ea6c6579c3ba3b754b92717
Maxim <panda.gtr007@gmail.com>
2022-10-31 23:45:11 +0600

    1

--- gulpfile.js
+++ gulpfile.js
@@ -20,7 +20,7 @@ import { reset } from "./gulp/tasks/reset.js";
 import { html } from "./gulp/tasks/html.js";
 import { server } from "./gulp/tasks/server.js";
 import { scss } from "./gulp/tasks/scss.js";
-import { js } from "./gulp/tasks/js.js";
+// import { js } from "./gulp/tasks/js.js";
 import { images } from "./gulp/tasks/images.js";
 import { otfToTtf, ttfToWoff, fontsStyle } from "./gulp/tasks/fonts.js";
 // import { svgSpriteTask } from "./gulp/tasks/svg-sprive.js";
@@ -30,9 +30,9 @@ import { ftp } from "./gulp/tasks/ftp.js";
 // Наблюдатель за изменениями в файлах
 function watcher() {
 	gulp.watch(path.watch.files, copy);
+	gulp.watch(path.watch.js, copy); //.js
 	gulp.watch(path.watch.html, html); //gulp.series(html, ftp) для каждого для выгрузки на сервер
 	gulp.watch(path.watch.scss, scss);
-	gulp.watch(path.watch.js, js);
 	gulp.watch(path.watch.images, images);
 }
 
@@ -40,7 +40,7 @@ function watcher() {
 const fonts = gulp.series(otfToTtf, ttfToWoff, fontsStyle);
 
 // Основные задачи
-const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, js, images)); //svgSpriteTask
+const mainTasks = gulp.series(fonts, gulp.parallel(copy, html, scss, images)); //svgSpriteTask и js
 
 // Построение сценариев выполнения задач
 const dev = gulp.series(reset, mainTasks, gulp.parallel(watcher, server));
