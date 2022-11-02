cb858e920548a61ba7130d6f71e3e2992c9e3545
Maxim <panda.gtr007@gmail.com>
2022-11-02 20:55:54 +0600

    create burger menu

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
     $('.header__burger').click(function(event){
-        $('.header__burger, .header__link').toggleClass('active');
+        $('.header__burger, .header__links, .header__row').toggleClass('active');
     })
 });
