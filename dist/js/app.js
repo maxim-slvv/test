ca4f5c8db082b603d4d7e71f127c27c0bac87878
Maxim <panda.gtr007@gmail.com>
2022-11-02 20:25:52 +0600

    1

--- dist/js/app.js
+++
@@ -1,2 +0,0 @@
-import * as flsFunctions from "./modules/functions.js";
-flsFunctions.isWebp();
--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,5 @@
-console.log('hello'); 
-$('.fade').slick({
-    dots: true,
-    infinite: true,
-    speed: 500,
-    fade: true,
-    cssEase: 'linear'
-  });
+$(document).ready(function(){
+    $('.header__burger').click(function(event){
+        $('.header__burger, .header__link').toggleClass('active');
+    })
+});
