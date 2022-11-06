3c0652b43ae37bd93b1f4ac9b6d87e4b68cbb814
Maxim <panda.gtr007@gmail.com>
2022-11-06 21:27:47 +0600

    add animations

--- dist/files/script.js
+++ dist/files/script.js
@@ -1,22 +1,29 @@
+//бургер меню
 $(document).ready(function(){
     $('.header__burger').click(function(event){
         $('.header__burger, .header__links, .header__row').toggleClass('active');
     })
 });
 
-var number = document.querySelectorAll('.way__number');
-
+// при наведении на пункты "направления деятельности" -
+// дает пункту анимацию
+var number = document.querySelectorAll('.way__item');
 var addMod = function (number){
     number.addEventListener('mouseenter', function(event){
-        console.log('red');
+        let sortedNumber = number;
+        sortedNumber.classList.add('pulse');
+});
+    number.addEventListener('mouseleave', function(event){
+        let sortedNumber = number;
+        sortedNumber.classList.remove('pulse');
 });
 };
 for(i = 0; i < number.length; i++){
     addMod(number[i]);
 }
 
-// $('way__number'+i).hover(function(){
-//     $('way__number1').addClass('hovered');
-// }, function() {
-//     $('way__number1').removeClass('hovered');
-// });
+// отменить стандартное действие кнопки отправки формы
+let button = document.querySelector('.form__button');
+button.addEventListener('click', function(event){
+    event.preventDefault();
+})
