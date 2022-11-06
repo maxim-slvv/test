039f55d2e1a1751f643040bb61ff3a69db181f93
Maxim <panda.gtr007@gmail.com>
2022-11-06 20:29:23 +0600

    add script hover

--- dist/files/script.js
+++ dist/files/script.js
@@ -3,3 +3,20 @@ $(document).ready(function(){
         $('.header__burger, .header__links, .header__row').toggleClass('active');
     })
 });
+
+var number = document.querySelectorAll('.way__number');
+
+var addMod = function (number){
+    number.addEventListener('mouseenter', function(event){
+        console.log('red');
+});
+};
+for(i = 0; i < number.length; i++){
+    addMod(number[i]);
+}
+
+// $('way__number'+i).hover(function(){
+//     $('way__number1').addClass('hovered');
+// }, function() {
+//     $('way__number1').removeClass('hovered');
+// });
